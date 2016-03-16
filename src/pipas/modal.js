e1c50aeda0a189841c38be9c1d68924028eac2f0
Petr Štipek <p.stipek@email.cz>
2016-03-16 09:19:16 +0100

    Added modal translations support

--- src/pipas/modal.js
+++ src/pipas/modal.js
@@ -11,19 +11,23 @@
             return '<button type="button" class="btn btn-default" ' + (closing ? 'data-dismiss="modal"' : '') + '>' + title + '</button>';
         };
         this.id = 'modal-' + Math.round(Math.random() * 10000);
+        this.text = {
+            close: 'Close',
+            refresh: 'Refresh'
+        };
         this.element = function () {
             if (!$elm) {
                 $elm = $('<div class="modal fade" id="' + this.id + '" tabindex="-1" role="dialog" aria-labelledby="' + this.id + '-label">'
                     + '<div class="modal-dialog" role="document">'
                     + '<div class="modal-content">'
                     + '<div class="modal-header">'
-                    + '<span class="modal-header-button close" data-dismiss="modal" aria-label="Close"><span class="fa fa-close"></span></span>'
-                    + '<a href="#" class="modal-header-button refresh"><span class="fa fa-refresh"></span></a>'
+                    + '<span class="modal-header-button close" data-dismiss="modal" aria-label="' + that.text.close + '"><span class="fa fa-close"></span></span>'
+                    + '<a href="#" class="modal-header-button refresh" title="' + that.text.refresh + '"><span class="fa fa-refresh"></span></a>'
                     + '<h4 class="modal-title" id="' + this.id + '-label"></h4>'
                     + '</div>'
                     + '<div class="modal-body"></div>'
                     + '<div class="modal-footer">'
-                    + createButton("Close", true)
+                    + createButton(that.text.close, true)
                     + '</div>'
                     + '</div>'
                     + '</div>'
@@ -165,7 +169,7 @@
                 });
             });
             //Button close
-            $footer.append(createButton("Close", true));
+            $footer.append(createButton(that.text.close, true));
         };
         /**
          * Set url for header refresh button
