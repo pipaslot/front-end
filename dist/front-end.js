83aad5122ef2a4ad3f0e4c96c7a08a0b66729ed6
Petr Štipek <p.stipek@email.cz>
2016-05-16 12:01:32 +0200

    Spinner replaced by font-awesome icon

--- dist/front-end.js
+++ dist/front-end.js
@@ -411,7 +411,7 @@ var pipas = (function ($) {
 
     pipas.spinner = new function () {
         var inner = {
-            defaultElement: $('<svg class="pipas-spinner-svg" viewBox="0 -256 1792 1792"><defs id="defs3033"/><g transform="matrix(1,0,0,-1,121.49153,1315.7966)" id="g3027"><path d="M 496,192 Q 496,132 453.5,90 411,48 352,48 q -60,0 -102,42 -42,42 -42,102 0,60 42,102 42,42 102,42 59,0 101.5,-42 Q 496,252 496,192 z M 928,0 Q 928,-53 890.5,-90.5 853,-128 800,-128 747,-128 709.5,-90.5 672,-53 672,0 672,53 709.5,90.5 747,128 800,128 853,128 890.5,90.5 928,53 928,0 z M 320,640 Q 320,574 273,527 226,480 160,480 94,480 47,527 0,574 0,640 q 0,66 47,113 47,47 113,47 66,0 113,-47 47,-47 47,-113 z M 1360,192 q 0,-46 -33,-79 -33,-33 -79,-33 -46,0 -79,33 -33,33 -33,79 0,46 33,79 33,33 79,33 46,0 79,-33 33,-33 33,-79 z M 528,1088 Q 528,1015 476.5,963.5 425,912 352,912 279,912 227.5,963.5 176,1015 176,1088 q 0,73 51.5,124.5 51.5,51.5 124.5,51.5 73,0 124.5,-51.5 Q 528,1161 528,1088 z m 464,192 q 0,-80 -56,-136 -56,-56 -136,-56 -80,0 -136,56 -56,56 -56,136 0,80 56,136 56,56 136,56 80,0 136,-56 56,-56 56,-136 z m 544,-640 q 0,-40 -28,-68 -28,-28 -68,-28 -40,0 -68,28 -28,28 -28,68 0,40 28,68 28,28 68,28 40,0 68,-28 28,-28 28,-68 z m -208,448 q 0,-33 -23.5,-56.5 -23.5,-23.5 -56.5,-23.5 -33,0 -56.5,23.5 -23.5,23.5 -23.5,56.5 0,33 23.5,56.5 23.5,23.5 56.5,23.5 33,0 56.5,-23.5 23.5,-23.5 23.5,-56.5 z" id="path3029" style="fill:currentColor"/> </g></svg>'),
+            defaultElement: $('<i></i>'),
             parents: {},
             getSelector: function (id) {
                 return (id && id != "body") ? "#" + id : "body";
@@ -421,8 +421,8 @@ var pipas = (function ($) {
                 var $parent = $(parentSelector).css("position", "relative");
                 var $spinner = $parent.find("> .pipas-spinner");
                 if (!$spinner.length) {
-                    $spinner = $('<div class="pipas-spinner"></div>');
-                    inner.defaultElement.clone().appendTo($spinner);
+                    $spinner = $('<i class="pipas-spinner" class="fa fa-spinner fa-pulse fa-lg fa-fw" style="color:white"></i>');
+                    //inner.defaultElement.clone().appendTo($spinner);
                     $spinner.appendTo($parent);
                 }
                 return $spinner;
@@ -1751,7 +1751,7 @@ var pipas = (function ($) {
  *
  * For disabling redirection apply class "no-redirect"
  */
-(function ($, document, PipasUrl) {
+(function ($, document, PipasUrl, PipasUtils) {
     $.nette.ext('redirect', false);
     $.nette.ext('redirect', {
         init: function () {
@@ -1776,10 +1776,10 @@ var pipas = (function ($) {
             if (settings.redirectUrl == undefined)settings.redirectUrl = true;
             if (settings.nette && settings.nette.el) {
                 var $elm = settings.nette.el;
-                if ($elm && $elm.hasClass("no-redirect")){
+                if ($elm && $elm.hasClass("no-redirect")) {
                     settings.redirect = false;
                 }
-                if ($elm && $elm.hasClass("no-redirect-url")){
+                if ($elm && $elm.hasClass("no-redirect-url")) {
                     settings.redirectUrl = false;
                 }
             }
@@ -1796,7 +1796,7 @@ var pipas = (function ($) {
                         document.location.href = payload.redirect;
                         return false;
                     } else {
-                        var clonedSettings = pipas.utils.tryClone(settings);
+                        var clonedSettings = PipasUtils.tryClone(settings);
                         clonedSettings.url = payload.redirect;
                         //AJAX redirect
                         $.nette.ajax(clonedSettings);
@@ -1840,7 +1840,7 @@ var pipas = (function ($) {
         }
 
     });
-})(jQuery, document, pipas.url);
+})(jQuery, document, pipas.url, pipas.utils);
 /*
  * Show spinner for every ajax requests
  *
